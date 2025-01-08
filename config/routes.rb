2cbc2bf2b128b7175ccd05aac83de45b14d7a555
Smoyacs <smoya.cs@gmail.com>
2025-01-08 16:48:00 -0300

    learning routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,41 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  
+  # 7 typical routes:
+  # Index - Shows all the records
+  # get "/products", to: "products#index"
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # # New - Renders a form for creating a new record
+  # get "/products/new", to: "products#new"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # # Create - Processes the new form submission, handling errors and creating the record
+  # post "/products", to: "products#create"
+
+  # # Show - Renders a specific record for viewing
+  # get "/products/:id", to: "products#show"
+
+  # # Edit - Renders a form for updating a specific record
+  # get "/products/:id/edit", to: "products#edit"
+
+  # # Update - Handles the edit form submission, handling errors and updating the record
+  # patch "/products/:id", to: "products#update"
+  # put "/products/:id", to: "products#update"
+
+  # # Destroy - Handles deleting a specific record
+  # delete "/products/:id", to: "products#destroy"
+
+  # resources :products
+  resources :products, only: [:index, :show]
+
+  # Controller namespaces and routing
+  namespace :admin do
+    resources :products
+  end
+
+  # Admin controller without prefix /admin
+  # scope module: "admin" do
+  #   resources :products
+  # end
+
 end
